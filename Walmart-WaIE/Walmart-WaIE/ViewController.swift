91a42b16062ec20554baa4b9187c05cde6f4fe68
kandavel-umapathy <kandaswamy197svg@gmail.com>
2023-03-02 15:22:16 +0530

    [WaIE] - GeneralSetup
    Created a general SetUp

---
+++ Walmart-WaIE/Walmart-WaIE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Walmart-WaIE
+//
+//  Created by kandavel on 02/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Walmart-WaIE/Walmart-WaIEUITests/Walmart_WaIEUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Walmart_WaIEUITestsLaunchTests.swift
+//  Walmart-WaIEUITests
+//
+//  Created by kandavel on 02/03/23.
+//
+
+import XCTest
+
+class Walmart_WaIEUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
